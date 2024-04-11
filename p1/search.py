db1d7499d56299e15da98ce152babcab6f21a93f
Erik Chao <etchao@ucsc.edu>
2024-04-11 11:16:05 -0700

    Adding DFS to repo

---
+++ p1/search.py
@@ -0,0 +1,73 @@
+"""
+In this file, you will implement generic search algorithms which are called by Pacman agents.
+"""
+from pacai.util.stack import Stack
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first [p 85].
+
+    Your search algorithm needs to return a list of actions that reaches the goal.
+    Make sure to implement a graph search algorithm [Fig. 3.7].
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+    ```
+    print("Start: %s" % (str(problem.startingState())))
+    print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
+    print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
+    ```
+    """
+
+    # *** Your Code Here ***
+    stack = Stack()
+    visited = list()
+    actions = list()
+    parent = {}
+    for travel in problem.successorStates(problem.startingState()):
+        stack.push(travel)
+
+    while not stack.isEmpty():
+        vertex = stack.pop()
+        print(problem.isGoal(vertex[0]))
+        if problem.isGoal(vertex[0]):
+            path = list()
+            while vertex is not None:
+                path.append(vertex[1])
+                vertex = parent.get(vertex)
+
+            return path[::-1]
+        if vertex not in visited:
+            visited.append(vertex)
+            for travel in problem.successorStates(vertex[0]):
+                if travel not in visited:
+                    print("%s" % (str(travel)))
+                    stack.push(travel)
+                    parent[travel] = vertex
+
+
+
+    return None
+
+def breadthFirstSearch(problem):
+    """
+    Search the shallowest nodes in the search tree first. [p 81]
+    """
+
+    # *** Your Code Here ***
+    raise NotImplementedError()
+
+def uniformCostSearch(problem):
+    """
+    Search the node of least total cost first.
+    """
+
+    # *** Your Code Here ***
+    raise NotImplementedError()
+
+def aStarSearch(problem, heuristic):
+    """
+    Search the node that has the lowest combined cost and heuristic first.
+    """
+
+    # *** Your Code Here ***
+    raise NotImplementedError()
